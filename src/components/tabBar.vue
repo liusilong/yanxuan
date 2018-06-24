6f24b08b0a688b7b6df604ab0a1f0a9f22fe8b3e
Liusilong <aaa1157867422@gmail.com>
2018-06-24 16:57:12 +0800

    init commit

---
+++ src/components/tabBar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="wrapper">
+        <div class="bar-item" @click="tabTo('home')">
+            <text class="bar-txt" :class="[this.isActive('home')]">首页</text>
+        </div>
+        <div class="bar-item" @click="tabTo('topic')">
+            <text class="bar-txt" :class="[this.isActive('topic')]">专题</text>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            indexKey: 'home'
+        }
+    },
+    methods: {
+        isActive(c) {
+            return this.indexKey === c ? 'bar-active' : ''
+        },
+        tabTo(tabValue) {
+            if(this.indexKey === tabValue) return;
+            this.indexKey = tabValue;
+            this.$emit('tabTo', {
+                status: 'tabTo',
+                data: {
+                    key: tabValue
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+    .wrapper {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 90px;
+        flex-wrap: nowrap;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        border-top-width: 1px;
+        border-top-color: #d9d9d9;
+        background-color: #fafafa;
+    }
+    .bar-active {
+        color: red
+    }
+    .bar-item {
+        flex: 1;
+    }
+    .bar-txt {
+        color: #666;
+        text-align: center;
+    }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <router-view/>
+    <tab-bar @tabTo = 'onTabTo'></tab-bar>
+  </div>
+</template>
+
+<script>
+import tabBar from './components/tabBar.vue';
+const modal = weex.requireModule('modal');
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    'tab-bar': tabBar
+  },
+  methods: {
+    onTabTo(result) {
+      modal.toast({
+        message: result.data.key
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
